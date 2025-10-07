9127daf4ee0d6fd2213d70c5b2b52eaf6b541b60
Nedixcode <kirilgrischa@mail.ru>
2025-10-07 18:30:20 +0300

    added main structure of project

---
+++ src/main/java/backend/shoppetproject/ShopPetProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.shoppetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/shoppetproject/ShopPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.shoppetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
